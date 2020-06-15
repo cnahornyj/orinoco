53e982ee3fc6a4589f6c4973a4b67fe936c52dbe
Clara Nahornyj <c.nahornyj@hotmail.fr>
2020-06-15 12:01:29 +0200

    Erreur de retour mais réponse du serveur attendue

--- js/test.js
+++ js/test.js
@@ -1,7 +1,6 @@
 const produitSell = "furniture";
 const APIURL = "http://localhost:3000/api/" + produitSell + "/";
 
-
 let idProduit = "";
 
 if (localStorage.getItem("userBasket")) {
@@ -21,7 +20,7 @@ let products = [];
 let userBasket = JSON.parse(localStorage.getItem("userBasket"));
 
 getProduits = () => {
-	return new Promise((resolve) =>{
+	return new Promise((resolve) => {
 		let request = new XMLHttpRequest();
 		request.onreadystatechange = function() {
 			if(this.readyState == XMLHttpRequest.DONE && this.status == 200) 
@@ -219,13 +218,13 @@ addition = () => {
 annulerProduit = (i) => {
     console.log(`Administration : Enlever le produit à l'index ${i}`);
     // Recupérer le array
-    userPanier.splice(i, 1); 
-    console.log(`Administration : ${userPanier}`);
+    userBasket.splice(i, 1); 
+    console.log(`Administration : ${userBasket}`);
     // Vider le localstorage
     localStorage.clear();
     console.log(`Administration : localStorage vidé`);
-    // Mettre à jour le localStorage avec le nouveau panier
-    localStorage.setItem('userPanier', JSON.stringify(userPanier));
+    // Mettre à jour le localStorage avec le nouveau Basket
+    localStorage.setItem('userBasket', JSON.stringify(userBasket));
     console.log(`Administration : localStorage mis à jour`);
     // Réactualiser la page avec le nouveau montant du panier/ou panier vide
     window.location.reload();
@@ -233,11 +232,9 @@ annulerProduit = (i) => {
 
 checkInput = () => {
     //Controle Regex
-    let checkString = /[a-zA-Z]/;
-    let checkNumber = /[0-9]/;
-    //Source pour vérification email => emailregex.com
-    let checkMail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/y;
-    let checkSpecialCharacter = /[§!@#$%^&*(),.?":{}|<>]/;
+    let checkString = /^[A-Z]{1}[a-z]/;
+    let checkMail = /.+@.+\..+/;
+    let checkAdresse = /^[^@&"()!_$*€£`%+=\/;?#]+$/;
 
     //message fin de controle
     let checkMessage = "";
@@ -250,40 +247,35 @@ checkInput = () => {
     let formVille = document.getElementById("formVille").value;
 
 
-    // tests des différents input du formulaire
-    // Test du nom => aucun chiffre ou charactère spécial permis
-    if(checkNumber.test(formNom) == true || checkSpecialCharacter.test(formNom) == true || formNom == ""){
-      checkMessage = "Vérifier/renseigner votre nom";
+    // Test des différents input du formulaire
+    if(checkString.test(formNom) == false){
+      checkMessage = "Pour votre nom : renseigner une majuscule puis des minuscules";
     }else{
       console.log("Administration : Nom ok");
     };
-    // Test du nom => aucun chiffre ou charactère spécial permis
-    if(checkNumber.test(formPrenom) == true || checkSpecialCharacter.test(formPrenom) == true || formPrenom == ""){
-      checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre prénom";
+    if(checkString.test(formPrenom) == false){
+      checkMessage = checkMessage + "\n" + "Pour votre prénom : renseigner une majuscule puis des minuscules";
     }else{
       console.log("Administration : Prénom ok");
     };
-    // Test du mail selon le regex de la source L256
     if(checkMail.test(formMail) == false){
-      checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre email";
+      checkMessage = checkMessage + "\n" + "Votre email doit être au format xxxx@yyy.zzz";
     }else{
       console.log("Administration : Adresse mail ok");
     };
-    // Test de l'adresse => l'adresse ne contient pas obligatoirement un numéro de rue mais n'a pas de characteres spéciaux
-    if(checkSpecialCharacter.test(formAdresse) == true || formAdresse == ""){
-      checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre adresse";
+    if(checkAdresse.test(formAdresse) == false){
+      checkMessage = checkMessage + "\n" + `Dans votre adresse : l'un ou plusieurs des caractères suivants sont interdits [^@&"()!_$*€£%+=`;
     }else{
       console.log("Administration : Adresse ok");
     };
-    // Test de la ville => aucune ville en France ne comporte de chiffre ou charactères spéciaux
-    if(checkSpecialCharacter.test(formVille) == true && checkNumber.test(formVille) == true || formVille == ""){
-      checkMessage = checkMessage + "\n" + "Vérifier/renseigner votre ville"
+    if(checkString.test(formVille) == false){
+      checkMessage = checkMessage + "\n" + "Pour le nom de votre ville : renseigner une majuscule puis des minuscules";
     }else{
       console.log("Administration : Ville ok")
     };
     // Si un des champs n'est pas bon => message d'alert avec la raison
     if(checkMessage != ""){
-      alert("Il est nécessaire de :" + "\n" + checkMessage);
+      alert("ATTENTION" + "\n" + checkMessage);
     } else {
       contact = {
         firstName: formNom,
@@ -292,97 +284,66 @@ checkInput = () => {
         city: formVille,
         email: formMail
       };
-    return contact;
+    return true;
     };
 };
 
 checkPanier = () => {
-  //Vérifier qu'il y ai au moins un produit dans le panier
+  // Vérifier qu'il y ait au moins un produit dans le panier
   let etatPanier = JSON.parse(localStorage.getItem("userBasket"));
-  //Si le panier est vide ou null (suppression localStorage par)=>alerte
+  // Si le panier est vide ou null (suppression localStorage par)=>alerte
   if(etatPanier == null){
-	//Si l'utilisateur à supprimer son localStorage etatPanier sur la page basket.html et qu'il continue le process de commande
+	//Si l'utilisateur à supprimer son localStorage etatPanier sur la page panier.html et qu'il continue le process de commande
 	alert("Il y a eu un problème avec votre panier, une action non autorisée a été faite. Veuillez recharger la page pour la corriger");
-	return false
-}else if(etatPanier.length < 1 || etatPanier == null){
-	console.log("Administration: ERROR => le localStorage ne contient pas de panier")
-	alert("Votre panier est vide");
 	return false;
-}else{
+  } else if(etatPanier.length < 1) {
+    alert("Votre panier est vide");
+    return false;
+  } else {
 	console.log("Administration : Le panier n'est pas vide")
-    //Si le panier n'est pas vide on rempli le products envoyé à l'API
-    JSON.parse(localStorage.getItem("userBasket")).forEach((produit) =>{
-    	products.push(produit._id);
-    });
-    console.log("Administration : Ce tableau sera envoyé à l'API : " + products)
-    return true;
+  // Si le panier n'est pas vide on remplit le products envoyé à l'API
+  JSON.parse(localStorage.getItem("userBasket")).forEach((produit) =>{
+    products.push(produit._id);
+  });
+  console.log("Administration : Le tableau envoyé à l'API contiendra les id de produit(s) suivant(s) : " + products)
+  return true;
   }
 };
 
-envoiDonnees = (objetRequest) => {
-  return new Promise((resolve) => { 
-  let request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-  if(this.readyState == XMLHttpRequest.DONE && this.status == 201) 
-  {
-    //Sauvegarde du retour de l'API dans la sessionStorage pour affichage dans order-confirm.html
-    sessionStorage.setItem("order", this.responseText);
-
-    //Chargement de la page de confirmation
-    document.forms["form-panier"].action = './order-confirm.html';
-    document.forms["form-panier"].submit();
-
-    resolve(JSON.parse(this.responseText));
-  }
-};
-request.open("POST", APIURL + "order");
-request.setRequestHeader("Content-Type", "application/json");
-request.send(objetRequest);
-});
-};
-
-validForm = () => {
-  // Ecoute de l'event click du formulaire
-  let btnForm = document.getElementById("envoiPost");
-  btnForm.addEventListener("click", function(){
-    //Lancement des verifications du panier et du form => si Ok envoi
-    if(checkPanier() == true && checkInput() != null){
-      console.log("Administration : L'envoi peut etre fait");
+//Au click sur le btn de validation du formulaire
+validForm = () => { 
+  //Ecoute de l'event click du formulaire
+  if(checkPanier() == true && checkInput() == true){
+    console.log("Administration : L'envoi peut etre fait");
     //Création de l'objet à envoyer
     let objet = {
       contact,
       products
     };
     console.log("Administration : " + objet);
-   // Conversion en JSON
-   let objetRequest = JSON.stringify(objet);
-   console.log("Administration : " + objetRequest);
-   // Envoi de l'objet via la function
-   envoiDonnees(objetRequest);
-
-   // Une fois la commande faite retour à l'état initial des tableaux/objet/localStorage
-   contact = {};
-   products = [];
-   localStorage.clear();
+    //Conversion en JSON
+    let objetRequest = JSON.stringify(objet);
+    console.log("Administration : " + objetRequest);
+    //Envoi de l'objet via la function
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if(this.readyState == XMLHttpRequest.DONE && this.status == 201) 
+      {
+        //Sauvegarde du retour de l'API dans la sessionStorage pour affichage dans order-confirm.html
+        sessionStorage.setItem("order", this.responseText);
+        resolve(JSON.parse(this.responseText));
+    } else {
+      alert("Problème avec l'envoi des données");
+    }
+    };
+    request.open("POST", APIURL + "order");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(objetRequest);
+    //Une fois la commande faite retour à l'état initial des tableaux/objet/localStorage
+    //contact = {};
+    //products = [];
+    //localStorage.clear();
   }else{
-  console.log("Administration : ERROR");
+    console.log("Administration : ERROR");
   };
-});
 };
-
-resultOrder = () => {
-  if(sessionStorage.getItem("order") != null){
-    // Parse du session storage
-    let order = JSON.parse(sessionStorage.getItem("order"));
-    // Implatation de prénom et de id de commande dans le html sur la page de confirmation
-    document.getElementById("lastName").innerHTML = order.contact.lastName;
-    document.getElementById("orderId").innerHTML = order.orderId; 
-    
-    // Suppression de la clé du sessionStorage pour renvoyer au else si actualisation de la page ou via url direct
-    sessionStorage.removeItem("order");
-  } else {
-    // Avertissement et redirection vers l'accueil
-    alert("Aucune commande passée, vous êtes arrivé ici par erreur");
-    window.open("./index.html");
-  }
-}
